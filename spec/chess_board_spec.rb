4b01a6657e7d1fd65fda8f3b6e55ff771782ce67
Malcolm Bailey <malcolm.bailey@spiff.com>
2023-06-13 19:34:54 -0400

    Fixed single test for chess_board #initialize

--- spec/chess_board_spec.rb
+++ spec/chess_board_spec.rb
@@ -1,9 +1,9 @@
-require 'chess_game'
+require_relative '../chess_board'
 
 RSpec.describe ChessBoard do
   describe '#initialize' do
+    board = ChessBoard.new.board
     it 'creates the correct starting board' do
-      board = ChessBoard.new
       expect(board[0].map(&:piece)).to eql(%i[rook knight bishop queen king bishop knight rook])
       expect(board[7].map(&:piece)).to eql(%i[rook knight bishop queen king bishop knight rook])
       expect(board[1].map(&:piece)).to eql(Array.new(8, :pawn))
