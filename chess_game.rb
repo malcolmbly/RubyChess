c772c1ca05658b4877a29723bf70f58a6c1038a7
Malcolm Bailey <malcolm.bailey@spiff.com>
2023-06-13 14:04:27 -0400

    Added reminder for display board method

--- chess_game.rb
+++ chess_game.rb
@@ -9,10 +9,14 @@ class ChessGame
     'TODO: map current position to available moves, filter out invalid moves, provide final list'
   end
 
+  def print_board
+    'TODO: print out board with unicode characters'
+  end
+
   def print_available_moves
     'TODO: Add prompting and providing possible moves'
   end
-  
+
   def game_over?
     'TODO: check for check + checkmate, force moves'
   end
